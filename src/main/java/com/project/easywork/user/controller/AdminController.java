5dcf44c1a828c518f895eb5897aed6def67e959b
Arang0109 <kmsq321@gmail.com>
2025-12-10 22:32:45 +0900

    remove: excel 내보내기 관련 파일 삭제 (#6)

--- src/main/java/com/project/easywork/user/controller/AdminController.java
+++ src/main/java/com/project/easywork/user/controller/AdminController.java
@@ -27,7 +27,7 @@ public class AdminController {
   @Operation(summary = "전체 회원조회 API", description = "전체 회원 목록을 조회합니다.")
   @PreAuthorize("hasRole('ADMIN')")
   @GetMapping("/users")
-  public ResponseEntity<ApiResponse<List<UserResponseDto>>> getUsers() {
-    return ResponseEntity.ok(ApiResponse.ok(userService.findAll()));
+  public ResponseEntity<ApiResponse<List<UserResponseDto>>> getList() {
+    return ResponseEntity.ok(ApiResponse.success(userService.findAll()));
   }
 }
